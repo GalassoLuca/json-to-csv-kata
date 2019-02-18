89c9f675fcab60398b9c349d7fe6035fa1c89447
Luca Galasso <luca.galasso@wonderflow.co>
2019-02-18 15:55:40 +0100

    fix: return empty string when arguments are undefined

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = (obj) => {
+  if (!obj) {
+    return ''
+  }
+
   const columns = Object.keys(obj[0]).join(',')
   const rows = obj.map(obj => Object.values(obj).join(',')).join('\n')
   return `${columns}\n${rows}`
